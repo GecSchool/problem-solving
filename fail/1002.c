1a97f290bb46f66a82c7cf0d67bf1d9d47b38816
GecSchool <grw7407@naver.com>
2022-04-18 00:09:44 +0900

    [Baek]1002

--- fail/1002.c
+++ fail/1002.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
-int main(void){
-    int t,x1,y1,r1,x2,y2,r2,len,r_sum;
-    scanf("%d",&t);
-    for(int i=0;i<t;i++){
-        scanf("%d %d %d %d %d %d",&x1,&y1,&r1,&x2,&y2,&r2);
-        if(x1==x2&&y1==y2&&r1==r2){
+#include <math.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int x1, y1, x2, y2, r1, r2, radiusSum;
+        double dotDistance;
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+        if (r2 > r1)
+        {
+            int tmp = x1;
+            x1 = x2;
+            x2 = tmp;
+            tmp = y1;
+            y1 = y2;
+            y2 = tmp;
+            tmp = r1;
+            r1 = r2;
+            r2 = tmp;
+        } // firstCircle has big radius
+        if (x1 == x2 && y1 == y2 && r1 == r2)
+        {
             printf("-1\n");
             continue;
         }
-        len=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
-        r_sum=(r1+r2)*(r1+r2);
-        if(len>r1*r1&&len>r2*r2){
-            if(len-r_sum>0){
-                printf("0\n");
-            }
-            else if(len-r_sum==0){
-                printf("1\n");
-            }
-            else{
-                printf("2\n");
-            }
+        radiusSum = r1 + r2;
+        dotDistance = sqrt((double)((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+        if ((radiusSum == dotDistance) || (r1 == dotDistance + r2))
+        {
+            printf("1\n");
         }
-        else{
-            if()
-            
-
+        else if ((dotDistance > radiusSum) || ((dotDistance < radiusSum) && (r1 > r2 + dotDistance)))
+        {
+            printf("0\n");
         }
+        else
+        {
+            printf("2\n");
+        }
+        // dot distance -> 두 점 사이에 거리
+        // r_sum -> 반지름 합
+        // 0개 -> dot distance > radius sum // dot distance < radius sum && big radius > dot distance + small radius
+        // 1개 -> dot distance == radius sum // big radius == dot distance + small radius
+        // 2개 -> else
+        // -1개 -> 모든것이 일치할 경우
     }
     return 0;
 }
